b10d8b0a378e71398fc92766face839dfcb60f31
Jun Jian Shen <jshen9085@gmail.com>
2019-01-22 22:36:53 -0500

    Java data types

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,7 +3,22 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        int myMinValue = -2147483648;
-        int myMaxValue = 2147483647;
+
+        // Int has a width of 32
+        int myMinValue = -2_147_483_648;
+        int myMaxValue = 2_147_483_647;
+        int myTotal = (myMinValue/2);
+        System.out.println("myTotal = " + myTotal);
+
+        // byte has a width of 8, must between -128 and 127
+        byte myByteValue = -128;
+        byte myNewByteValue = (byte) (myByteValue/2);
+        System.out.println("myNewByteValue = " + myNewByteValue);
+
+        // short has a width of 16, max is 32767, min is 32768
+        short myShortValue = 32767;
+
+        // long has a width of 64
+        long myLongValue = 100L;
     }
 }
